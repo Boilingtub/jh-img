6f31a9cca11e143b66b30ae96f9ad05c0ee39619
Boilingtub <74053750+Boilingtub@users.noreply.github.com>
2024-08-28 11:43:08 +0200

    Delete src directory

--- src/Window/window.h
+++
@@ -1,9 +0,0 @@
-#ifndef INCLUDE_WINDOW_H
-#define INCLUDE_WINDOW_H
-
-#include "../glad/glad.h"
-#include "GLFW/glfw3.h"
-
-GLFWwindow* create_new_window(const char Title[], int width, int height);
-
-#endif
